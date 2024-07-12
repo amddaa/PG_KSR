613eaa9fd2202fe650bf702a16a9da3d43bd3395
amddaa <slusarekadam1@gmail.com>
2024-07-12 22:49:46 +0200

    Create navbart

---
+++ Frontend/src/components/navbar.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Link from "next/link";
+import {CircleUser,} from "lucide-react";
+import {Button} from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const Navbar: React.FC = () => {
+    return (
+        <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+            <nav
+                className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+                <Link href="/" className="flex items-center gap-2 text-lg font-semibold md:text-base">
+                    <span className="text-xl">🚊</span>
+                </Link>
+                <Link href="/" className="text-foreground transition-colors hover:text-foreground">
+                    Connections
+                </Link>
+                <Link href="/" className="text-muted-foreground transition-colors hover:text-foreground">
+                    todo
+                </Link>
+            </nav>
+            <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+                <form className="ml-auto flex-1 sm:flex-initial"></form>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="secondary" size="icon" className="rounded-full">
+                            <CircleUser className="h-5 w-5"/>
+                            <span className="sr-only">Toggle user menu</span>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator/>
+                        <DropdownMenuItem>Settings</DropdownMenuItem>
+                        <DropdownMenuItem>Support</DropdownMenuItem>
+                        <DropdownMenuSeparator/>
+                        <DropdownMenuItem>Logout</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+        </header>
+    );
+};
+
+export default Navbar;
