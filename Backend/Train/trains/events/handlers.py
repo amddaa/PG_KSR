494f2c8aab31bd77b6fa9de6ac2423f0f9d4daed
amddaa <slusarekadam1@gmail.com>
2024-09-04 22:52:18 +0200

    Add more info to the event

--- Backend/Train/trains/events/handlers.py
+++ Backend/Train/trains/events/handlers.py
@@ -1,6 +1,8 @@
 import pika
+import uuid
 from django.conf import settings
 from rest_framework.utils import json
+from datetime import datetime
 
 
 class TrainEventHandler:
@@ -8,17 +10,25 @@ class TrainEventHandler:
         rabbitmq_uri = f"amqp://{settings.RABBITMQ_USER}:{settings.RABBITMQ_PASSWORD}@{settings.RABBITMQ_HOST}:{settings.RABBITMQ_PORT}/"
         self.rabbitmq_connection = pika.BlockingConnection(pika.URLParameters(rabbitmq_uri))
         self.rabbitmq_channel = self.rabbitmq_connection.channel()
-        self.rabbitmq_channel.exchange_declare(exchange='events', exchange_type='fanout')
+        self.rabbitmq_channel.exchange_declare(exchange='events', exchange_type='topic')
 
     def publish_event(self, event_type, event_data, stream_name):
+        event_id = str(uuid.uuid4())
+        timestamp = datetime.utcnow().isoformat()
+
         message = {
+            'event_id': event_id,
             'event_type': event_type,
             'stream_name': stream_name,
             'data': event_data,
+            'timestamp': timestamp,
         }
+
+        routing_key = f"{event_type}.{stream_name}"
+
         self.rabbitmq_channel.basic_publish(
             exchange='events',
-            routing_key='',
+            routing_key=routing_key,
             body=json.dumps(message)
         )
 
